e0bbcffd0b1019289f83c3061e6e2e84a7e7ca46
Sandor Martin Leyva <sandormartin37@gmail.com>
2021-11-27 16:12:34 -0500

    Add selected prop

--- client/src/components/Gateway.js
+++ client/src/components/Gateway.js
@@ -5,13 +5,15 @@ import styled from "styled-components";
 const GatewayComponent = styled.button`
   border: none;
   text-decoration: none;
-  background-color: white;
   width: 80%;
   height: 50px;
   padding: 5px;
   margin: 15px;
   border-radius: 10px;
   box-shadow: 0px 2px 5px 2px #dbdbdb;
+  margin-left: ${(props) => (props.selected ? "30px" : "15px")};
+  background-color: ${(props) => (props.selected ? "#5baf4c21" : "white")};
+
   transition: 0.3s;
   &:hover {
     box-shadow: 0px 2px 5px 2px #c9c9c9;
@@ -22,9 +24,9 @@ const IpContainer = styled.div`
   font-size: smaller;
 `;
 
-function Gateway({ name, ip, onClick }) {
+function Gateway({ name, ip, onClick, selected }) {
   return (
-    <GatewayComponent onClick={onClick}>
+    <GatewayComponent selected={selected} onClick={onClick}>
       <div>{name}</div>
       <IpContainer>{ip}</IpContainer>
     </GatewayComponent>
@@ -34,7 +36,8 @@ function Gateway({ name, ip, onClick }) {
 Gateway.propTypes = {
   name: PropTypes.string.isRequired,
   ip: PropTypes.string.isRequired,
-  onClick: PropTypes.func
+  onClick: PropTypes.func,
+  selected: PropTypes.bool
 };
 
 export default Gateway;
