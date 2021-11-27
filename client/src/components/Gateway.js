6c6f00101fe158c3577d429903d208e81dfdff26
Sandor Martin Leyva <sandormartin37@gmail.com>
2021-11-27 13:20:39 -0500

    Put button instead of div

--- client/src/components/Gateway.js
+++ client/src/components/Gateway.js
@@ -2,40 +2,39 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const GatewayComponent = styled.div`
+const GatewayComponent = styled.button`
+  border: none;
+  text-decoration: none;
+  background-color: white;
   width: 80%;
   height: 50px;
   padding: 5px;
   margin: 15px;
   border-radius: 10px;
   box-shadow: 0px 2px 5px 2px #dbdbdb;
+  transition: 0.3s;
+  &:hover {
+    box-shadow: 0px 2px 5px 2px #c9c9c9;
+  }
 `;
-
-// const Row = styled.div`
-//   div {
-//     display: inline;
-//   }
-// `;
 
 const IpContainer = styled.div`
-font-size: smaller;
+  font-size: smaller;
 `;
 
-function Gateway({name, ip}) {
+function Gateway({ name, ip, onClick }) {
   return (
-    <GatewayComponent>
-      {/* <Row> */}
-        <div>{name}</div>
-        <IpContainer>{ip}</IpContainer>
-      {/* </Row> */}
+    <GatewayComponent onClick={onClick}>
+      <div>{name}</div>
+      <IpContainer>{ip}</IpContainer>
     </GatewayComponent>
   );
 }
 
-
 Gateway.propTypes = {
   name: PropTypes.string.isRequired,
   ip: PropTypes.string.isRequired,
+  onClick: PropTypes.func
 };
 
 export default Gateway;
